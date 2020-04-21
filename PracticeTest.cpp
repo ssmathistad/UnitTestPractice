d7aff3c024ae969905763acdbc1c64dd029febac
ssmathistad <ssmathistad@csuchico.edu>
2020-04-21 12:49:22 -0700

    Added custom palindrome tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -25,5 +25,40 @@ TEST(PracticeTest, is_not_a_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Ba");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, single_char_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, case_insensitive_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Bab");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, not_palindrome_from_start_to_end)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("aab");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, racecar_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, tattarrattat_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("tattarrattat");
     ASSERT_TRUE(actual);
 }
