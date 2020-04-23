f48a0bb8c900e4fc06ea8812d7ae604e428c0c99
ssmathistad <ssmathistad@csuchico.edu>
2020-04-22 19:10:09 -0700

    Fix additional variable type errors

--- Practice.cpp
+++ Practice.cpp
@@ -42,7 +42,7 @@ bool Practice::isPalindrome(string input)
   }
   for(int i=0; i < (int)input.size()/2; i++)
   {
-    if( input[i] != input[input.size()-1-i] )
+    if( input[i] != input[(int)input.size()-1-i] )
       return false;
   }
   return true;
